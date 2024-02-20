7e9e4f5a359ade228da3e573eb4d32be3a821656
pandohansamuel19 <pandohansamuel19@gmail.com>
2024-02-20 21:04:08 +0700

    [server]: CRUD Successfully

--- third_party/notion.go
+++ third_party/notion.go
@@ -1,11 +1,15 @@
 package thirdparty
 
-func NotionAuth() {
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/spf13/viper"
+)
 
+func NotionAuth(c echo.Context) {
 }
 
 func NotionDataSync() {
-
+	viper.AddConfigPath(".env/")
 }
 
 func NotionUpdateData() {
