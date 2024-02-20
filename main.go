a87bfbbea0932866ee7bebabe194838706b36ab7
pandohansamuel19 <pandohansamuel19@gmail.com>
2024-02-20 17:54:25 +0700

    [readme]: add documentations

--- main.go
+++ main.go
@@ -1,12 +1,7 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/smkdev-id/promotion_tracking_dashboard/models"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("HELLO WORLD !!")
-	fmt.Printf("models.HelloFromModels(): %v\n", models.HelloFromModels())
+	fmt.Println("Hello World")
 }
